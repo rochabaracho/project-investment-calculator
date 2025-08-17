2380183cd892712eb5c99ada1d9459b4d95d4021
rochabaracho <lbaracho71@gmail.com>
2025-08-17 14:33:29 -0300

    25.14. Associating the Action to the Submit Event

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import { generateReturnsArray } from "./src/investmentGoals.js";
 
-const calculateButton = document.getElementById("calculate-results");
+const form = document.getElementById("investment-form");
 
-function renderProgression() {
+function renderProgression(event) {
+  event.preventDefault();
   const startingAmount = Number(
     document.getElementById("starting-amount").value
   );
@@ -27,4 +28,4 @@ function renderProgression() {
   console.log(returnsArray);
 }
 
-calculateButton.addEventListener("click", renderProgression);
+form.addEventListener("submit", renderProgression);
