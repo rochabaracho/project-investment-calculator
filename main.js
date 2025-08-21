26366c86cbcfa6744f8d7ae90da94e2f196db650
rochabaracho <lbaracho71@gmail.com>
2025-08-21 13:39:34 -0300

    the-intelligence-of-the-progression-chart

--- main.js
+++ main.js
@@ -7,6 +7,54 @@ const clearFormButton = document.getElementById("clear-form");
 const finalMoneyChart = document.getElementById("final-money-distribution");
 const progressionChart = document.getElementById("progression");
 
+function validateInput(event) {
+  if (event.target.value === "") {
+    return;
+  }
+
+  // const parentElement = event.target.parentElement;
+  const { parentElement } = event.target;
+  const grandParentElement = event.target.parentElement.parentElement;
+  const inputValue = event.target.value.replace(",", ".");
+
+  if (
+    !parentElement.classList.contains("error") &&
+    (isNaN(inputValue) || Number(inputValue) < 0)
+  ) {
+    parentElement.classList.add("error");
+    const errorParagraphElement = document.createElement("p");
+    errorParagraphElement.classList.add("font-bold");
+    errorParagraphElement.classList.add("text-red-500");
+    errorParagraphElement.innerText =
+      "Enter a numeric value greater than zero.";
+    grandParentElement.appendChild(errorParagraphElement);
+  } else {
+    if (parentElement.classList.contains("error")) {
+      parentElement.classList.remove("error");
+      grandParentElement.querySelector("p").remove();
+    }
+  }
+}
+
+function formatCurrency(value) {
+  return value.toFixed(2);
+}
+
+function clearForm() {
+  form["starting-amount"].value = "";
+  form["additional-contributions"].value = "";
+  form["time-amount"].value = "";
+  form["return-rate"].value = "";
+  form["tax-rate"].value = "";
+
+  const errorInputContainers = document.querySelectorAll(".error");
+
+  for (const errorInputContainer of errorInputContainers) {
+    errorInputContainer.classList.remove("error");
+    errorInputContainer.parentElement.querySelector("p").remove();
+  }
+}
+
 function renderProgression(event) {
   event.preventDefault();
   if (document.querySelector(".error")) {
@@ -39,14 +87,25 @@ function renderProgression(event) {
     returnRatePeriod
   );
 
+  const finalInvestmentObject = returnsArray[returnsArray.length - 1];
+
+  const dataChart = [
+    formatCurrency(finalInvestmentObject.investedAmount),
+    formatCurrency(
+      finalInvestmentObject.totalInterestReturns * (1 - taxRate / 100)
+    ),
+    formatCurrency(
+      finalInvestmentObject.totalInterestReturns * (taxRate / 100)
+    ),
+  ];
+
   new Chart(finalMoneyChart, {
     type: "doughnut",
     data: {
-      labels: ["Red", "Blue", "Yellow"],
+      labels: ["Total Invested", "Income", "Tax"],
       datasets: [
         {
-          label: "My First Dataset",
-          data: [300, 50, 100],
+          data: [dataChart[0], dataChart[1], dataChart[2]],
           backgroundColor: [
             "rgb(255, 99, 132)",
             "rgb(54, 162, 235)",
@@ -58,51 +117,44 @@ function renderProgression(event) {
     },
   });
 
+  new Chart(progressionChart, {
+    type: "bar",
+    data: {
+      labels: returnsArray.map((investmentObject) => investmentObject.month),
+      datasets: [
+        {
+          label: "Total Investment",
+          data: returnsArray.map((investmentObject) =>
+            formatCurrency(investmentObject.investedAmount)
+          ),
+          backgroundColor: "rgb(255, 99, 132)",
+        },
+        {
+          label: "Investment Profitability",
+          data: returnsArray.map((investmentObject) =>
+            formatCurrency(investmentObject.interestReturns)
+          ),
+          backgroundColor: "rgb(54, 162, 235)",
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      interaction: {
+        intersect: false,
+      },
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
+    },
+  });
+
   console.log(returnsArray);
-}
-
-function clearForm() {
-  form["starting-amount"].value = "";
-  form["additional-contributions"].value = "";
-  form["time-amount"].value = "";
-  form["return-rate"].value = "";
-  form["tax-rate"].value = "";
-
-  const errorInputContainers = document.querySelectorAll(".error");
-
-  for (const errorInputContainer of errorInputContainers) {
-    errorInputContainer.classList.remove("error");
-    errorInputContainer.parentElement.querySelector("p").remove();
-  }
-}
-
-function validateInput(event) {
-  if (event.target.value === "") {
-    return;
-  }
-
-  // const parentElement = event.target.parentElement;
-  const { parentElement } = event.target;
-  const grandParentElement = event.target.parentElement.parentElement;
-  const inputValue = event.target.value.replace(",", ".");
-
-  if (
-    !parentElement.classList.contains("error") &&
-    (isNaN(inputValue) || Number(inputValue) < 0)
-  ) {
-    parentElement.classList.add("error");
-    const errorParagraphElement = document.createElement("p");
-    errorParagraphElement.classList.add("font-bold");
-    errorParagraphElement.classList.add("text-red-500");
-    errorParagraphElement.innerText =
-      "Enter a numeric value greater than zero.";
-    grandParentElement.appendChild(errorParagraphElement);
-  } else {
-    if (parentElement.classList.contains("error")) {
-      parentElement.classList.remove("error");
-      grandParentElement.querySelector("p").remove();
-    }
-  }
 }
 
 for (const formElement of form) {
