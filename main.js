2a80cc942f4cbfb608574755c6ed170a28110a3f
rochabaracho <lbaracho71@gmail.com>
2025-08-22 11:17:24 -0300

    update branch

--- main.js
+++ main.js
@@ -184,5 +184,5 @@ for (const formElement of form) {
   }
 }
 
-form.addEventListener("submit", renderProgression);
+// form.addEventListener("submit", renderProgression);
 clearFormButton.addEventListener("click", clearForm);
